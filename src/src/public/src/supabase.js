3d6c918373f89c51b8f84ddf28d88ac083cee3ed
chauhasir-lab <chauha.sir@gmail.com>
2026-04-25 13:43:50 +0530

    Add Supabase client initialization in supabase.js

---
+++ src/src/public/src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "cxjmwjljjnuthcrmnbqb";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4am13amxqam51dGhjcm1uYnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwOTYzMzUsImV4cCI6MjA5MjY3MjMzNX0.ynRaceAT1uWcsPOJ_5vY_8NKolM3EaWKQajUEk4sLz8";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
